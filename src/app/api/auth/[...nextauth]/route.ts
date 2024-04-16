f17195a6e350ed4d4d0836211d5dfee87e7004e8
Minh <beaver700nh@gmail.com>
2024-04-16 07:59:49 -0400

    More auth progress!!!

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,39 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
-// const handler = NextAuth({
-//   providers: [
-//     Credentials({
-//       credentials: {
-//         username: {},
-//         password: {},
-//       },
-//       async authorize(credentials) {
-//         // temp
-//       },
-//     }),
-//   ],
-//   pages: {
-//     signIn: "/login",
-//   },
-// });
+import { compare } from "bcrypt";
 
-// export { handler as GET, handler as POST };
+import { loginSchema } from "@/app/lib/schema";
+
+const handler = NextAuth({
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        username: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const parsedCredentials = loginSchema.safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          throw new Error("Invalid format for login credentials");
+        }
+
+        const { username, password } = parsedCredentials.data;
+        const user = { id: "SOME_ID", password: "$2a$12$0ZyLyY/B3ToUGC9feMEUPOfgqFIQKVMrNtJOMbjhYvYYBQDjd34uy" };
+
+        if (!await compare(password, user.password)) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,13 @@
 
 import Image from "next/image";
 
+import { signIn } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Box, Button, Icon, MenuItem, TextField, Typography, alpha, useTheme } from "@mui/material";
 
 import { LoginSchema, loginSchema } from "@/app/lib/schema";
-import { login } from "@/app/lib/auth";
 
 export default function Login() {
   const theme = useTheme();
@@ -17,14 +17,30 @@ export default function Login() {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-    reset,
     setError,
   } = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
   });
 
   const onSubmit = async (data: LoginSchema) => {
-    login(data);
+    const res = await signIn("credentials", {
+      ...data,
+      redirect: false,
+    });
+
+    if (res == null) {
+      setError("root", {
+        message: "Failed to reach the server",
+      });
+    }
+    else if (res.ok) {
+      console.log("SUCCESS!");
+    }
+    else {
+      setError(res.error === "Incorrect password" ? "password" : "root", {
+        message: res.error ?? "An unknown error occurred",
+      });
+    }
   };
 
   return (
@@ -42,18 +58,14 @@ export default function Login() {
         style={{
           width: "calc(25lvw + 25lvh)",
           backgroundColor: alpha(theme.palette.background.default, 0.75),
-          grid:
-            '"icon title"    min-content' +
-            '"user user"     min-content' +
-            '"pass pass"     min-content' +
-            '"submit submit" min-content' +
-            '/ min-content 1fr',
+          gridTemplateRows: "repeat(auto-fit, min-content)",
+          gridTemplateColumns: "min-content 1fr",
         }}
-        className="!min-w-min max-w-full !m-auto p-8 grid justify-items-start items-center gap-4 border-2 rounded-lg shadow-xl"
+        className="!min-w-min max-w-full !m-auto p-8 gap-4 grid justify-items-start items-center border-2 rounded-lg shadow-xl"
         onSubmit={handleSubmit(onSubmit)}
       >
         <Icon
-          sx={{ gridArea: "icon" }}
+          sx={{ gridColumn: "1" }}
           className="relative !text-[3rem]"
         >
           <Image
@@ -65,13 +77,13 @@ export default function Login() {
           />
         </Icon>
         <Typography
-          sx={{ gridArea: "title" }}
+          sx={{ gridColumn: "2" }}
           variant="h5"
         >
           Sign In
         </Typography>
         <TextField
-          sx={{ gridArea: "user" }}
+          sx={{ gridColumn: "1 / span 2" }}
           className="w-full"
           label="Settlement"
           name="username"
@@ -87,7 +99,7 @@ export default function Login() {
           <MenuItem value="4">Test Item D</MenuItem>
         </TextField>
         <TextField
-          sx={{ gridArea: "pass" }}
+          sx={{ gridColumn: "1 / span 2" }}
           className="w-full"
           label="Password"
           type="password"
@@ -97,7 +109,7 @@ export default function Login() {
           helperText={errors.password?.message}
         />
         <Button
-          sx={{ gridArea: "submit" }}
+          sx={{ gridColumn: "1 / span 2" }}
           className="w-full !normal-case"
           variant="contained"
           type="submit"
@@ -106,6 +118,13 @@ export default function Login() {
         >
           Next
         </Button>
+        {errors.root && <Typography
+          sx={{ gridColumn: "1 / span 2", color: theme.palette.error.main }}
+          className="w-full !-my-2"
+          variant="caption"
+        >
+          {errors.root.message}
+        </Typography>}
       </form>
     </Box>
   )
