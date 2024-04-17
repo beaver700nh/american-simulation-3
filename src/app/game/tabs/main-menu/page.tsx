cd2cd45dceeb7e85481ed5c2dc3a7c6d3aa33990
Minh <beaver700nh@gmail.com>
2024-04-17 14:23:17 -0400

    Adding main menu page

--- src/app/game/tabs/main-menu/page.tsx
+++ src/app/game/tabs/main-menu/page.tsx
@@ -1,7 +1,39 @@
+import { Box, Divider, Stack, Typography } from "@mui/material";
 export { House as Icon } from "@mui/icons-material";
 
 export const index = 0;
 
 export default function MainMenu() {
-  return <p>Main Menu</p>;
+  return (
+    <Box
+      className="grow flex relative"
+    >
+      <Stack
+        className="m-auto text-center"
+      >
+        <Typography
+          variant="h4"
+        >
+          &ldquo;The Sim&rdquo;
+        </Typography>
+        <Typography
+          variant="h6"
+        >
+          US I - American History Through Simulation
+        </Typography>
+        <Divider
+          className="!-mx-8 !my-4"
+          orientation="horizontal"
+          flexItem
+        />
+        <Typography
+          fontStyle="italic"
+          variant="body1"
+        >
+          Originally written by Mr. White in Microsoft Excel<br />
+          Ported to the web by his high school student Minh Lê
+        </Typography>
+      </Stack>
+    </Box>
+  )
 }
